5af706dea8341767c942de5997fcbac855d5f7ae
Roveir Datu <roveirdatu@yahoo.comgit config --global user.email roveirdatu@yahoo.com>
2018-09-11 22:15:24 -0700

    updated css, html, js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log ('hello');
+ var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F'];
+ var memory_values = [];
+ var memory_tile_ids = [];
+ var tiles_flipped = 0;
+
+ 	Array.prototype.memory_tile_shuffle = function(){
+			var i = this.length, j, temp;
+			while (--i > 0) {
+				j=Math.floor(Math.random() * (i+1));
+				temp = this[j];
+				this[j] = this[i];
+				this[i] = temp;
+			}
+		}
+
+
+		function newBoard(){
+			tiles_flipped = 0;
+			var output = '';
+			memory_array.memory_tile_shuffle();
+			for(var i=0; i < memory_array.length; i++) {
+				output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+			}
+			document.getElementById('memory_board').innerHTML = output;
+		}
+
+
+newBoard();	
